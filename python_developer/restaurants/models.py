47bfad2ee0e9904bba47f155b8bec06db2880812
Mikel Losada <mikel.losada@gmail.com>
2020-06-28 10:56:42 +0200

    Fix popularity and satisfaction rate fields not having enough digits, and make more fields nullable

--- python_developer/restaurants/models.py
+++ python_developer/restaurants/models.py
@@ -8,7 +8,8 @@ class Segment(models.Model):
     size = models.IntegerField()
     average_popularity_rate = models.FloatField(null=True)
     average_satisfaction_rate = models.FloatField(null=True)
-    average_price = models.DecimalField(max_digits=10, decimal_places=2)
+    average_price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    total_reviews = models.IntegerField(null=True)
 
     def __str__(self):
         return self.name
@@ -22,8 +23,8 @@ class Restaurant(models.Model):
     longitude = models.DecimalField(max_digits=10, decimal_places=7)
     latitude = models. DecimalField(max_digits=10, decimal_places=7)
     city_name = models.TextField()
-    popularity_rate = models.DecimalField(max_digits=2, decimal_places=2, null=True)
-    satisfaction_rate = models.DecimalField(max_digits=2, decimal_places=2, null=True)
+    popularity_rate = models.DecimalField(max_digits=4, decimal_places=2, null=True)
+    satisfaction_rate = models.DecimalField(max_digits=4, decimal_places=2, null=True)
     total_reviews = models.IntegerField()
     average_price = models.DecimalField(max_digits=10, decimal_places=2)
 
