349322c6b17b9975e4e15e3bbcde5d6516f28f2b
Mikel Losada <mikel.losada@gmail.com>
2020-06-28 15:11:03 +0200

    Add related_name='restaurants' to Restaurant.segments

--- python_developer/restaurants/models.py
+++ python_developer/restaurants/models.py
@@ -28,7 +28,7 @@ class Restaurant(models.Model):
     total_reviews = models.IntegerField()
     average_price = models.DecimalField(max_digits=10, decimal_places=2)
 
-    segments = models.ManyToManyField(Segment)
+    segments = models.ManyToManyField(Segment, related_name="restaurants")
     
     def __str__(self):
         return self.name
